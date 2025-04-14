cc7080a0d398b4fe3aa82daf6052dadcbc164b40
l1nza <112642007+LlnZa@users.noreply.github.com>
2025-04-14 15:31:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,13 @@ from forms import AddEmployeeForm
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/barbershop_db?client_encoding=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'SECRET_KEY=your_secret_key
+SQLALCHEMY_DATABASE_URI=postgresql://hair_salon_user:ERa7fcyODXXoU7Nck30fEY9HJGejcSzN@dpg-cvue38a4d50c73atiucg-a/hair_salon_8vaf
+MAIL_SERVER=smtp.gmail.com
+MAIL_PORT=587
+MAIL_USERNAME=your_email@gmail.com
+MAIL_PASSWORD=your_password
+MAIL_USE_TLS=True '
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Конфигурация Flask-Mail
